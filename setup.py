3d4b8db8a1aa1328d203bd9ba9c8ff2213532571
ajmaths <105535062+ajmaths@users.noreply.github.com>
2025-08-10 22:50:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="cycle_reps_package",
     version="0.1.0",
     author="Wenwen Li",
+    author_email="wli11uco@gmail.com",
     description="Weighted bipartite graph package with spanning tree and cycle analysis",
     packages=find_packages(),
     install_requires=[
